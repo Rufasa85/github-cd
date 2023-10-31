073c52f6b4128d4b7a8aac1eedb853d1771c579b
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2023-10-31 10:26:55 -0700

    first deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <h1>Hello, automatic deployment!</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
